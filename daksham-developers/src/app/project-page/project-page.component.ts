cdf29ba64b7b0b573c3f4faa417164733b5f38b0
Bhavik Solanki <solankibhavikp@gmail.com>
2024-05-26 02:25:13 +0530

    Richtext for Location Advantage

--- daksham-developers/src/app/project-page/project-page.component.ts
+++ daksham-developers/src/app/project-page/project-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
 import { ContentfulService } from '../service/contentful.service';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,31 +12,32 @@ import { Observable } from 'rxjs';
   standalone: true,
   imports: [CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './project-page.component.html',
-  styleUrl: './project-page.component.css'
+  styleUrls: ['./project-page.component.css']
 })
-export class ProjectPageComponent implements OnInit {
+export class ProjectPageComponent implements OnInit, OnDestroy {
 
   projects: any | undefined;
-
-  constructor(private route: ActivatedRoute, private contentfulService: ContentfulService) { }
-
   projectData$!: Observable<any>;
+  richTextHtml: string | undefined;
   isMobileView = false;
   activeDotIndex = 0;
   private intervalId: any;
 
+  constructor(private route: ActivatedRoute, private contentfulService: ContentfulService) { }
+
   ngOnInit(): void {
-    this.route.params.subscribe(
-      params => {
-        const projectId = params['id'];
-        this.projectData$ = this.contentfulService.getProjectById(projectId);
-      }
-    );
+    this.route.params.subscribe(params => {
+      const projectId = params['id'];
+      this.projectData$ = this.contentfulService.getProjectById(projectId);
+      this.projectData$.subscribe(projectData => {
+        if (projectData?.fields.locationAdvantage) {
+          this.richTextHtml = documentToHtmlString(projectData.fields.locationAdvantage);
+        }
+      });
+    });
 
     this.checkViewport();
     this.startAutoSlide();
-
-
   }
 
   @HostListener('window:resize', ['$event'])
@@ -84,5 +86,4 @@ export class ProjectPageComponent implements OnInit {
     this.stopAutoSlide();
     this.startAutoSlide(); // Reset the interval when manually changing slides
   }
-
 }
