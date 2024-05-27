b6802c92628e6e86c290bbf862d18fe08de003f9
Bhavik Solanki <solankibhavikp@gmail.com>
2024-05-27 18:41:19 +0530

    Update Carousel for Homepage Component with CoreUI Library

--- daksham-developers/src/app/home/home.component.ts
+++ daksham-developers/src/app/home/home.component.ts
@@ -1,23 +1,27 @@
-import { Component, Input, HostListener, OnInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 import { ContentfulService } from '../service/contentful.service';
 import { Entry } from 'contentful';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ProjectPageComponent } from '../project-page/project-page.component';
 import { AboutUsComponent } from '../about-us/about-us.component';
-//import { CarouselComponent } from '../carousel/carousel.component';
+import { ThemeDirective, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, CarouselItemComponent, CarouselControlComponent } from '@coreui/angular';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent, FooterComponent, ProjectPageComponent, AboutUsComponent],
+  imports: [CommonModule, RouterModule, NavbarComponent, FooterComponent, ProjectPageComponent, AboutUsComponent, ThemeDirective, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, NgFor, CarouselItemComponent, CarouselControlComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+  project: any;
+  projectData: any;
+  richTextHtml: any;
 
   constructor(private contentfulService: ContentfulService) { }
 
@@ -41,7 +45,7 @@ export class HomeComponent implements OnInit {
       console.log('Projects :', this.projects);
     });
     this.checkViewport();
-    this.startAutoSlide();
+
   }
 
   @HostListener('window:resize', ['$event'])
@@ -49,37 +53,8 @@ export class HomeComponent implements OnInit {
     this.checkViewport();
   }
 
-  ngOnDestroy() {
-    this.stopAutoSlide();
-  }
-
   checkViewport() {
     this.isMobileView = window.innerWidth < 768; // Assuming 768px is the breakpoint for mobile devices
   }
 
-  startAutoSlide() {
-    this.intervalId = setInterval(() => {
-      this.nextSlide();
-    }, 5000); // Change slide every 5 seconds
-  }
-
-  stopAutoSlide() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-
-  nextSlide() {
-    this.activeDotIndex = (this.activeDotIndex + 1) % this.projects.length;
-  }
-
-  prevSlide() {
-    this.activeDotIndex = (this.activeDotIndex - 1 + this.projects.length) % this.projects.length;
-  }
-
-  goToSlide(index: number) {
-    this.activeDotIndex = index;
-    this.stopAutoSlide();
-    this.startAutoSlide(); // Reset the interval when manually changing slides
-  }
 }
